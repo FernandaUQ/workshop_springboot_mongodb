c7df82c1b1bbe1aee5b4c2705876ec72674d239d
Fernanda Uchoa <nanda.uq015@gmail.com>
2020-01-22 14:58:46 -0300

    primeiro commit Projeto Criado

---
+++ src/test/java/com/nandauq/workshopmongo/WorkshopSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandauq.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
