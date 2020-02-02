e23ac7374f2a562b5a86d702e2f008075ba7b292
Fernanda Uchoa <nanda.uq015@gmail.com>
2020-02-02 13:18:58 -0300

    Obtendo um usuário por ID

--- src/main/java/com/nandauq/workshopmongo/services/UserService.java
+++ src/main/java/com/nandauq/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.nandauq.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nandauq.workshopmongo.domain.User;
 import com.nandauq.workshopmongo.repository.UserRepository;
+import com.nandauq.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,11 @@ public class UserService {
 		return repo.findAll();
 		
 	}
+	
+	public User findById (String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
 }
