cdbba70bd0ffbd0186fe81a8236d137004886d16
Fernanda Uchoa <nanda.uq015@gmail.com>
2020-02-10 11:46:20 -0300

    Obtendo post por id

---
+++ src/main/java/com/nandauq/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.nandauq.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nandauq.workshopmongo.domain.Post;
+import com.nandauq.workshopmongo.repository.PostRepository;
+import com.nandauq.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired // instancia automaticamente
+	private PostRepository repo;
+	
+	
+	public Post findById (String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
+	
