36e9a08ad405a347e7f702b7c2b7b50a42342e79
Fernanda Uchoa <nanda.uq015@gmail.com>
2020-02-10 11:37:21 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/nandauq/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nandauq/workshopmongo/resources/UserResource.java
@@ -3,19 +3,18 @@ package com.nandauq.workshopmongo.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.nandauq.workshopmongo.domain.Post;
 import com.nandauq.workshopmongo.domain.User;
 import com.nandauq.workshopmongo.dto.UserDTO;
 import com.nandauq.workshopmongo.services.UserService;
@@ -33,6 +32,8 @@ public class UserResource {
 		List <UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	
 	
 	@RequestMapping (value = "/{id}", method=RequestMethod.GET) //ou getmmaping
 	public ResponseEntity <UserDTO> findById (@PathVariable String id){	
@@ -62,6 +63,12 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.noContent().build();
 		}
+	
+	@RequestMapping (value = "/{id}/posts", method=RequestMethod.GET) //ou getmmaping
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){	
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 	
 	
 }
